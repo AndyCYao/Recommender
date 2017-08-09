430ffd6ac44d427c55b2583a0a8afe9acd32056d
AndyCYao <andyyao3@gmail.com>
2017-08-08 22:59:45 -0400

    Used AJAX Jquery to update the dropdown box

--- app.py
+++ app.py
@@ -11,19 +11,24 @@ def main():
 	return render_template('index.html', 
 					       option_list=reader_list)
 
-
-@app.route("/echo", methods=['POST'])
-def echo(): 
-    return render_template('index.html', text=request.form['text'])
+@app.route("/TestJquery", methods=['POST'])
+def get_input():
+	print("In TestQuery")
+	# data = request.
+	# return "You Selected {}".format(data)
+	pass 
 
 @app.route("/get_recommender", methods=['POST'])
 def get_recommender():
-	# print("In get_recommender")
-	customerID = request.form['option']
-	# print("Test Test {}".format(customerID))
-	recommended_list = rcmdr.getRec_Items(customerID)
-	return render_template('index.html', tables=[recommended_list])
-	# return render_template('index.html', recommended_list=recommended_list)
+	customerID = request.data
+	print("customer ID is {} type is {}".format(customerID, type(customerID)))
+	if(customerID != ''):
+		recommended_list = rcmdr.getRec_Items(customerID)
+		return recommended_list
+		#return render_template('index.html', tables=[recommended_list])
+	else:
+		return "Please Try Again"
+		#return render_template('index.html', message = "Please Try Again")
 
 if __name__ == '__main__':
 	app.run(debug = True)
